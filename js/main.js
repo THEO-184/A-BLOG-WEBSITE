a421f67e339c8dbe31402545c2191e371f1a90d2
Theophilus Boakye Yiadom <theophilusboakye47@gmail.com>
2021-06-18 10:34:34 +0100

    added AOS

--- js/main.js
+++ js/main.js
@@ -16,6 +16,10 @@ const responsive = {
 	},
 };
 
+window.addEventListener("load", () => {
+	AOS.init();
+});
+
 $(document).ready(function () {
 	$nav = $(".nav");
 	$toglleCollapse = $(".toggle-collapse");
@@ -46,27 +50,4 @@ $(document).ready(function () {
 	});
 });
 
-// SLIDE IN ANIMATION
 
-const sliderImages = document.querySelectorAll(".slide-in");
-
-function checkSlide(e) {
-	sliderImages.forEach((sliderImage) => {
-		// half way through the image
-		const slideInAt =
-			window.pageYOffset + window.innerHeight - sliderImage.height / 2;
-		// bottom of the image
-		const imageBottom = sliderImage.offsetTop + sliderImage.height;
-
-		const isHalfShown = slideInAt > sliderImage.offsetTop;
-		const isNotScrolledPast = window.pageYOffset < imageBottom;
-
-		if (isHalfShown && isNotScrolledPast) {
-			sliderImage.classList.add("active");
-		} else {
-			sliderImage.classList.remove("active");
-		}
-	});
-}
-
-window.addEventListener("scroll", checkSlide);
